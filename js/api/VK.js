0545f7d27efd73ec075d27150843fa93a9759fba
Antonio-87 <davydenko.87@list.ru>
2024-04-22 17:03:43 +0300

    env

--- js/api/VK.js
+++ js/api/VK.js
@@ -5,16 +5,15 @@
  * Имеет свойства ACCESS_TOKEN и lastCallback
  * */
 class VK {
-
-  static ACCESS_TOKEN = '446fd935f8b8db44469bc06e4de9deac4f9ad0ff4e68c15cdecfcb0d12a6b573ce3d8f7b8623946e43a29';
+  static ACCESS_TOKEN = process.env.VK_TOKEN;
   static lastCallback;
 
   /**
    * Получает изображения
    * */
-  static get(id = '', callback){
+  static get(id = "", callback) {
     this.lastCallback = callback;
-    let script = document.createElement('script');
+    let script = document.createElement("script");
     script.src = `https://api.vk.com/method/photos.get?owner_id=${id}&album_id=profile&access_token=${this.ACCESS_TOKEN}&v=5.131&callback=VK.processData`;
     document.getElementsByTagName("body")[0].appendChild(script);
   }
@@ -23,21 +22,26 @@ class VK {
    * Передаётся в запрос VK API для обработки ответа.
    * Является обработчиком ответа от сервера.
    */
-  static processData(result){
-    document.querySelector('body').lastElementChild.remove();
-    if (result['error'] == undefined) {
-      let imgList = result.response.items
+  static processData(result) {
+    document.querySelector("body").lastElementChild.remove();
+    if (result["error"] == undefined) {
+      let imgList = result.response.items;
       let urlList = [];
-      imgList.forEach(element => {
-          let bigsize = element['sizes'][element['sizes'].length - 1]['url'];
-          if (bigsize) {
-            urlList.push(bigsize);
-            }
-        });
-        this.lastCallback(urlList);
+      imgList.forEach((element) => {
+        let bigsize = element["sizes"][element["sizes"].length - 1]["url"];
+        if (bigsize) {
+          urlList.push(bigsize);
+        }
+      });
+      this.lastCallback(urlList);
     } else {
-        alert('Ошибка :' + result['error']['error_code'] + '-' + result['error']['error_msg']);
+      alert(
+        "Ошибка :" +
+          result["error"]["error_code"] +
+          "-" +
+          result["error"]["error_msg"]
+      );
     }
     this.lastCallback = () => {};
-  };
+  }
 }
